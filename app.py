bf4d0ed30b7cf28abbc2d356c05ac29d3ea3f883
William Cole <williamcole@Williams-MacBook-Pro-3.local>
2020-02-15 16:31:04 +0000

    refactor

--- app.py
+++ app.py
@@ -2,46 +2,43 @@ import os, time
 import cv2
 import numpy as np
 
-def check_for_idle_activity():
-    while 1:
-        time.sleep(5)
-        cmd = "ioreg -c IOHIDSystem | perl -ane 'if (/Idle/) {$idle=(pop @F)/1000000000; print $idle}'"
-        result = os.popen(cmd)
-        str = result.read()
-        idle_time = int(str.split(".")[0])
-        print('user idle time', idle_time)
+while True:
+    time.sleep(1)
+    cmd = "ioreg -c IOHIDSystem | perl -ane 'if (/Idle/) {$idle=(pop @F)/1000000000; print $idle}'"
+    result = os.popen(cmd)
+    str = result.read()
+    idle_time = int(str.split(".")[0])
+    print('user idle time', idle_time)
+    seconds_face_not_detected = 0
 
-        if idle_time >= 10:
-            print('going to check if teh user is sitting at their computer')
+    if idle_time >= 10:
+        while True:
             face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-            cap = cv2.VideoCapture(0) 
-            face_not_visible_x_seconds = 0
+            cap = cv2.VideoCapture(0)
+            ret, img = cap.read()
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+            print(1)
 
-            while True:
-                ret, img = cap.read()
-                gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-                faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+            for (x, y, w, h) in faces:
+                print('FACE DETECTED')
+                seconds_face_not_detected = 0
+                # cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+                cap.release()
+                cv2.destroyAllWindows()
 
-                for (x, y, w, h) in faces:
-                    cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-                    print('face detected')
-                    # cap.release()
-                    # cv2.destroyAllWindows()
-                    face_not_visible_x_seconds = 0
-
-                cv2.imshow('img', img)
-
-                if face_not_visible_x_seconds != 30:
-                    print('number of seconds face not visible', face_not_visible_x_seconds)
-                    face_not_visible_x_seconds += 1
-                else:
-                    print('LOCK THE COMPUTER')
+            else:
+                seconds_face_not_detected += 1
+                print('seconds_face_not_detected', seconds_face_not_detected)
+                if seconds_face_not_detected > 10:
+                    print('LOCK SCREEN')
                     sleep_cmd = """osascript -e 'ignoring application responses' -e 'tell application "Finder" to sleep' -e end"""
                     os.system(sleep_cmd)
-                k = cv2.waitKey(30) & 0xff
-                if k == 27:
                     break
-            cap.release()
-            cv2.destroyAllWindows()
 
-check_for_idle_activity()
+            # cv2.imshow('img', img)
+
+            k = cv2.waitKey(30) & 0xff
+            if k == 27:
+                break
+
